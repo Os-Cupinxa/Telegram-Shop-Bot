9234fec47154992867937fe030666f5c9e46afb6
samuelt005 <samuel.thomas1109@outlook.com>
2024-09-07 12:57:28 -0300

    Criado base da API com FastAPI e SQLAlchemy

---
+++ server/README.md
@@ -0,0 +1,17 @@
+# Como rodar o servidor
+
+## Instalar dependências:
+
+```bash
+pip install -r requirements.txt
+```
+
+## Rodar o servidor FastAPI:
+
+```bash
+uvicorn app.main:app --reload
+```
+
+## Swagger do FastAPI:
+
+[Swagger FastAPI](http://127.0.0.1:8000/docs)
---
+++ server/app/controllers/user_controller.py
@@ -0,0 +1,18 @@
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.orm import Session
+from app.schemas.user_schema import UserCreate, UserResponse
+from app.services import user_service
+from app.config.database import get_db
+
+router = APIRouter()
+
+@router.post("/users/", response_model=UserResponse)
+def create_user(user: UserCreate, db: Session = Depends(get_db)):
+    return user_service.create_user(db, user)
+
+@router.get("/users/{user_id}", response_model=UserResponse)
+def read_user(user_id: int, db: Session = Depends(get_db)):
+    db_user = user_service.get_user(db, user_id)
+    if db_user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    return db_user
