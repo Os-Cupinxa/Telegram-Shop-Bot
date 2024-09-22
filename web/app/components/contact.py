de74fa7038fceebb158dada56bb52bdd461a63ee
Antony Henrique Bresolin <40371352+AntonyBresolin@users.noreply.github.com>
2024-09-22 18:38:03 -0300

    feat: initial structure of front end
    Estrutura inicial do front end da admin page com django

--- web/app/components/contact.py
+++
@@ -1,5 +0,0 @@
-import streamlit as st
-
-def show_contact():
-    st.subheader("Contact Page")
-    st.write("You can contact us at contact@example.com.")
--- web/app/components/sidebar.py
+++
@@ -1,5 +0,0 @@
-import streamlit as st
-
-
-def show_sidebar():
-    st.sidebar.selectbox("Menu", ["Home", "About", "Contact"])
