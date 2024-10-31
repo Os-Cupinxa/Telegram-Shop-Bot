1bc2c10a11357abcc8d42fb60719f9642b78d991
Antony Henrique Bresolin <40371352+AntonyBresolin@users.noreply.github.com>
2024-10-30 21:51:48 -0300

    fix: added a pk in the product model

--- web/app/frontend/models.py
+++ web/app/frontend/models.py
@@ -7,6 +7,7 @@ class Category(models.Model):
 
 
 class Product(models.Model):
+    id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     description = models.TextField()
