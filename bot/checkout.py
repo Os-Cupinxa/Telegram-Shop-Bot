2b081584c0136f0a8db9d076d14bfd3c3d6f6d81
Samuel <samuel.thomas1109@outlook.com>
2024-11-08 21:58:13 -0300

    Add order confirmation step with inline keyboard

--- bot/checkout.py
+++ bot/checkout.py
@@ -1,9 +1,31 @@
-from telegram import Update
+from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import ContextTypes
 import httpx
 
+from utils import format_date
+
 
 async def checkout(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    order_message = (
+        f" *Deseja confirmar o pedido?*\n"
+    )
+
+    keyboard = [
+        [InlineKeyboardButton("✅ Confirmar", callback_data="go_to-confirm_checkout"),
+         InlineKeyboardButton("❌ Voltar", callback_data="go_to-cart")],
+    ]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+
+    if update.callback_query:
+        await update.callback_query.message.reply_text(order_message, parse_mode='Markdown', reply_markup=reply_markup)
+    else:
+        await update.message.reply_text(order_message, parse_mode='Markdown', reply_markup=reply_markup)
+
+
+async def confirm_order(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    query = update.callback_query
+    await query.answer()
+
     cart = context.user_data.get('cart', [])
     user_info = context.user_data.get('user_info', {})
 
@@ -16,16 +38,19 @@ async def checkout(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
         "items": cart_items
     }
 
+    print(order_data)
+
     async with httpx.AsyncClient() as client:
         response = await client.post("http://127.0.0.1:8001/orders/", json=order_data)
 
     if response.status_code == 200:
         order_response = response.json()
+        formated_date = format_date(order_response['created_date'])
 
         message = (
             f"🎉 *Pedido criado com sucesso!*\n\n"
             f"🆔 *Número do Pedido*: {order_response['id']}\n"
-            f"📅 *Data do Pedido*: {order_response['created_date']}\n"
+            f"📅 *Data do Pedido*: {formated_date}\n"
             f"💵 *Total*: R$ {order_response['amount']}\n"
             f"📦 *Status*: {order_response['status']}\n\n"
         )
@@ -35,9 +60,11 @@ async def checkout(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
         else:
             await update.message.reply_text(message, parse_mode="Markdown")
     else:
+        print(response.status_code)
+        print(response.text)
         error_message = (
-            f"❌ *Erro ao criar pedido!*\n\n"
-            f"Por favor tente novamente mais tarde!\n"
+            f"❌ *Erro ao salvar seu pedido!*\n\n"
+            f"Por favor, tente novamente mais tarde!\n"
         )
 
         if update.callback_query:
