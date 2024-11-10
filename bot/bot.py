17a1d443f29da6428dde017ca63f20d1b70b13ab
samuelt005 <samuel.thomas1109@outlook.com>
2024-11-10 10:37:31 -0300

    Add order handling features

--- bot/bot.py
+++ bot/bot.py
@@ -14,6 +14,7 @@ from account import log_in, check_user_by_cpf, choose_info_to_edit, show_user_in
 from cart import show_cart, handle_quantity, add_to_cart, prompt_remove_item, confirm_remove_from_cart
 from catalogue import show_catalogue_categories, get_products, navigate_product
 from checkout import checkout, confirm_order
+from orders import get_orders, navigate_order, get_order_details
 from registering import process_name, process_cpf, process_phone, process_city, process_address
 
 # Enable logging
@@ -51,6 +52,9 @@ async def go_to(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
 
     elif page == "profile":
         await show_user_info(update, context)
+
+    elif page == "orders":
+        await get_orders(update, context)
 
     elif page == "login":
         await log_in(update, context)
@@ -127,7 +131,9 @@ def main() -> None:
     application.add_handler(CallbackQueryHandler(go_to, pattern=r'go_to-.*'))
     application.add_handler(CallbackQueryHandler(get_products, pattern=r'show_products-.*'))
     application.add_handler(CallbackQueryHandler(navigate_product, pattern=r'prev_product|next_product'))
+    application.add_handler(CallbackQueryHandler(navigate_order, pattern=r'prev_order|next_order'))
     application.add_handler(CallbackQueryHandler(add_to_cart, pattern=r'add_to_cart-.*'))
+    application.add_handler(CallbackQueryHandler(get_order_details, pattern=r'see_order_details-.*'))
     application.add_handler(CallbackQueryHandler(prompt_remove_item, pattern=r'prompt_remove_item'))
     application.add_handler(CallbackQueryHandler(confirm_remove_from_cart, pattern=r'confirm_remove_item-.*'))
 
---
+++ bot/orders.py
@@ -0,0 +1,145 @@
+from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import ContextTypes
+import httpx
+
+from utils import format_date
+
+
+async def get_orders(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    query = update.callback_query
+    await query.answer()
+    client_id = context.user_data.get('user_info', {}).get('id')
+
+    if not client_id:
+        await query.message.reply_text("ID do cliente não encontrado.")
+        return
+
+    url = f"http://127.0.0.1:8001/orders/client/{client_id}"
+
+    async with httpx.AsyncClient() as orders_client:
+        response = await orders_client.get(url)
+
+    if response.status_code != 200:
+        await query.message.reply_text("Erro ao buscar pedidos.")
+        return
+
+    orders_data = response.json()
+
+    context.user_data['orders'] = orders_data
+    context.user_data['current_order_index'] = 0
+
+    await display_order(query, context)
+
+
+async def display_order(query: Update.callback_query, context: ContextTypes.DEFAULT_TYPE) -> None:
+    orders = context.user_data.get('orders', [])
+    current_index = context.user_data.get('current_order_index', 0)
+
+    if not orders:
+        await query.message.reply_text("Nenhum pedido encontrado.")
+        return
+
+    order = orders[current_index]
+    formated_date = format_date(order['created_date'])
+
+    order_text = (
+        f"🆔 *Número do Pedido*: {order['id']}\n"
+        f"📅 *Data do Pedido*: {formated_date}\n"
+        f"💵 *Total*: R$ {order['amount']:.2f}\n"
+        f"📦 *Status*: {order['status']}\n\n"
+    )
+
+    navigation_buttons = []
+    if current_index > 0:
+        previous_button = InlineKeyboardButton("⬅️ Pedido Anterior", callback_data=f"prev_order")
+        navigation_buttons.append(previous_button)
+
+    if current_index < len(orders) - 1:
+        next_button = InlineKeyboardButton("Próximo Pedido ➡️", callback_data=f"next_order")
+        navigation_buttons.append(next_button)
+
+    see_order_details_button = InlineKeyboardButton("🔎 Ver Detalhes", callback_data=f"see_order_details-{order['id']}")
+    profile_button = InlineKeyboardButton("🔙 Voltar", callback_data=f"go_to-profile")
+
+    keyboard = []
+    if navigation_buttons:
+        keyboard.append(navigation_buttons)
+    keyboard.append([see_order_details_button, profile_button])
+
+    reply_markup = InlineKeyboardMarkup(keyboard)
+
+    # TODO VER ERRO QUE OCORRE AQUI AO TROCAR O PEDIDO
+    await query.edit_message_text(order_text, reply_markup=reply_markup, parse_mode='Markdown')
+
+
+async def navigate_order(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    query = update.callback_query
+    await query.answer()
+
+    current_index = context.user_data.get('current_order_index', 0)
+
+    if "prev_order" in query.data:
+        current_index -= 1
+    elif "next_order" in query.data:
+        current_index += 1
+
+    context.user_data['current_order_index'] = current_index
+
+    await display_order(update.callback_query, context)
+    await display_order(query, context)
+
+
+async def get_order_details(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    query = update.callback_query
+    await query.answer()
+
+    orders = context.user_data.get('orders', [])
+    current_index = context.user_data.get('current_order_index', 0)
+    order_id = orders[current_index]['id'] if orders else None
+
+    if not order_id:
+        await query.message.reply_text("Pedido não encontrado.")
+        return
+
+    url = f"http://127.0.0.1:8001/orders/items/{order_id}"
+
+    async with httpx.AsyncClient() as client:
+        response = await client.get(url)
+
+    if response.status_code != 200:
+        await query.message.reply_text("Erro ao buscar detalhes do pedido.")
+        return
+
+    items_data = response.json()
+
+    order = orders[current_index]
+    formated_date = format_date(order['created_date'])
+
+    order_text = (
+        f"🆔 *Número do Pedido*: {order['id']}\n"
+        f"📅 *Data do Pedido*: {formated_date}\n"
+        f"💵 *Total*: R$ {order['amount']:.2f}\n"
+        f"📦 *Status*: {order['status']}\n\n"
+        "📜 *Itens do Pedido:*\n"
+        f"--------------------------------------------------\n"
+    )
+
+    for item in items_data:
+        product = item['product']
+        quantity = item['quantity']
+        unit_price = product['price']
+        total_price = unit_price * quantity
+
+        order_text += (
+            f"🛍️ *Produto:* {product['name']}\n"
+            f"🔢 *Quantidade:* {quantity}\n"
+            f"💵 *Valor unitário:* R${unit_price:.2f}\n"
+            f"💰 *Total:* R${total_price:.2f}\n"
+            f"--------------------------------------------------\n"
+        )
+
+    back_button = InlineKeyboardButton("🔙 Voltar aos Pedidos", callback_data="go_to-orders")
+    keyboard = [[back_button]]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+
+    await query.edit_message_text(order_text, reply_markup=reply_markup, parse_mode='Markdown')
