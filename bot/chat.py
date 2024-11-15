897649581eb55c2d7c0250064a555c753b603fdf
Samuel <samuel.thomas1109@outlook.com>
2024-11-14 21:31:38 -0300

    Integrate httpx for async message posting

--- bot/chat.py
+++ bot/chat.py
@@ -1,3 +1,4 @@
+import httpx
 from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import ContextTypes
 
@@ -20,4 +21,17 @@ async def start_chat(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None
 
 async def save_message(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     message = update.message.text
-    print(message)
+    user_info = context.user_data.get('user_info', {})
+    client_id = user_info['id']
+
+    new_message_data = {
+        "chat_id": 1,
+        "message": message,
+        "client_id": client_id,
+    }
+
+    print(new_message_data)
+
+    async with httpx.AsyncClient() as client:
+        response = await client.post("http://127.0.0.1:8001/messages/", json=new_message_data)
+        print(response.json())
