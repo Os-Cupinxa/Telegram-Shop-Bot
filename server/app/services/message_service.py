9660526f438d24c3b7344320dfb86175aa737e5a
samuelt005 <samuel.thomas1109@outlook.com>
2024-11-16 16:24:30 -0300

    Update message handling with client and user distinctions

--- server/app/services/message_service.py
+++ server/app/services/message_service.py
@@ -2,6 +2,7 @@ import asyncio
 
 from sqlalchemy.orm import Session
 
+from app.models import Client, User
 from app.models.message_model import Message
 from app.schemas.message_schema import MessageCreate
 from app.services.global_service import get_object_by_id
@@ -18,6 +19,11 @@ def get_message(db: Session, message_id: int):
 
 
 def create_message(db: Session, message: MessageCreate):
+    if message.client_id is None:
+        user = get_object_by_id(db, User, message.user_id, "User not found")
+    else:
+        client = get_object_by_id(db, Client, message.client_id, "Client not found")
+
     db_message = Message(
         chat_id=message.chat_id,
         created_date=message.created_date,
@@ -30,27 +36,27 @@ def create_message(db: Session, message: MessageCreate):
     db.refresh(db_message)
 
     if message.client_id is None:
-        asyncio.run(send_to_bot(db_message))
+        asyncio.run(send_to_bot(db_message, user.name))
     else:
-        asyncio.run(send_to_client(db_message))
+        asyncio.run(send_to_client(db_message, client))
 
     return db_message
 
 
-async def send_to_bot(db_message: Message):
+async def send_to_bot(db_message: Message, name: str):
     from app.main import sio
     await sio.emit("new_message_to_bot", {
         "chat_id": db_message.chat_id,
         "message": db_message.message,
-        "user_id": db_message.user_id
+        "user_name": name
     })
 
-async def send_to_client(db_message: Message):
+async def send_to_client(db_message: Message, client: Client):
     from app.main import sio
     await sio.emit("new_message_to_web", {
         "chat_id": db_message.chat_id,
         "message": db_message.message,
-        "client_id": db_message.user_id
+        "client": client
     })
 
 
