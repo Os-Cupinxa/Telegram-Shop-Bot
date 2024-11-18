778cd6da04eeb754cdf5e88d57680971ff522067
Samuel <samuel.thomas1109@outlook.com>
2024-11-18 20:28:48 -0300

    Remove debug print statement from database configuration

--- server/app/config/database.py
+++ server/app/config/database.py
@@ -20,7 +20,6 @@ def create_database_if_not_exists():
 
 create_database_if_not_exists()
 
-print(DATABASE_HOST)
 DATABASE_URL = f"mysql+mysqlconnector://{DATABASE_USER}:{DATABASE_PASSWORD}@{DATABASE_HOST}:{DATABASE_PORT}/{DATABASE_NAME}"
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
